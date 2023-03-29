4657f62516ef07158afbccaa9d2cb6ae1ea88ba8
ductxfinatech <ductx@finatech.io>
2023-03-29 19:07:15 +0700

    add ui

--- src/components/films/list-popular-reviews.jsx
+++ src/components/films/list-popular-reviews.jsx
@@ -27,13 +27,44 @@ export default function FilmsListPopularReviews() {
         userName: "selena69",
         userId: 2,
       },
-      content:
-        "John Wick's biggest enemy was those damn stairs",
+      content: "John Wick's biggest enemy was those damn stairs",
       rate: 4,
       totalComment: 265,
       totalLike: 3654,
       isLiked: false,
     },
   ];
-  return <Box className="films-list-review"></Box>;
+
+  return (
+    <Box className="films-list-popular-review">
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderBottom: "1px solid #9ab",
+          marginBottom: "16px",
+          fontSize: "14px",
+        }}
+      >
+        <Box sx={{ textTransform: "uppercase" }}>Popular reviews this week</Box>
+        <Box></Box>
+      </Box>
+      {listReviews.map((review) => (
+        <Box sx={{ display: "flex", alignItems: "start" }}>
+          <Box sx={{ width: { xs: "60px", md: "80px" }, aspectRatio: "2/3", overflow: 'hidden', marginRight: '20px' }}>
+            <Box
+              component="img"
+              src={review.film.thumbnail}
+              alt="Image-review-film"
+              sx={{ width: '100%', minHeight: '100%'  }}
+            ></Box>
+          </Box>
+          <Box sx={{ flex: "1" }}>
+            <Box sx={{marginBottom: '8px'}}><Box sx={{ fontSize: '28px', fontWeight: '700', marginRight: '8px', display: 'inline-block' }}>{review.film.name}</Box><Box sx={{display: 'inline-block'}}>{review.film.yearRelease}</Box></Box>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
 }
