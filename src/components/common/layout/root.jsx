f9395d3b2f9e556ea3204df926d2c8fbdadf779e
hiepbanguyen <nguyenbahiep001@gmail.com>
2023-05-26 23:06:22 +0700

    add scroll to top of page when switching routes

--- src/components/common/layout/root.jsx
+++ src/components/common/layout/root.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Layout from "./layout.jsx";
 import UserStore from "../../../store/user.store.js";
 import React from "react";
 
 export default function Root() {
   // console.log("render root");
+  const location = useLocation();
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   React.useEffect(() => {
     // console.log("render root in useEffect");
     UserStore.loadFromLocal(); //get user info from local
